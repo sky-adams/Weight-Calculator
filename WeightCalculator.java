7556741c053c727bf152b7d01dbfd0ac9e8835d2
Sky Adams <skyadams@sbunified.org>
2017-09-01 11:19:22 -0700

    updated

--- WeightCalculator.java
+++ WeightCalculator.java
@@ -4,7 +4,14 @@ public class WeightCalculator
 	{
 		double weight = Double.parseDouble(args[0]);
 		double mars = weight * 0.38;
+		double jupiter = weight * 2.36;
+		double venus = weight * 0.91;
+		double saturn = weight * 1.06;
+		
 		System.out.println("Mars: " + mars);
-		System.out.println("Jupiter: " + weight * 2.36);
+		System.out.println("Jupiter: " + jupiter);
+		System.out.println("Venus: " + venus);
+		System.out.println("Saturn: " + saturn);
 	}
+	
 }
